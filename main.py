05350c9f1db4559adee6361e82e9133c7f7d9dde
Aryan Karadia <aryan.karadia@ucalgary.ca>
2022-11-06 17:39:26 -0700

    movement of players

--- main.py
+++ main.py
@@ -10,19 +10,27 @@ BLACK = (0, 0, 0)
 
 FPS = 60
 
-player1_img = pygame.image.load(os.path.join('assets', 'player.png'))
-player2_img = pygame.image.load(os.path.join('assets', 'player.png'))
-ball_img = pygame.image.load(os.path.join('assets', 'ball.png'))
+VEL = 5
 
-def draw_window():
+background_img = pygame.image.load(os.path.join("assets", "background.png"))
+player1_img = pygame.image.load(os.path.join('assets', 'player.png'))
+player1_img = pygame.transform.scale(player1_img, (25, 50))
+player2_img = pygame.image.load(os.path.join('assets', 'player.png'))
+player2_img = pygame.transform.scale(player2_img, (25, 50))
+ball_img = pygame.image.load(os.path.join('assets', 'ball.png'))
+ball_img = pygame.transform.scale(ball_img, (20, 20))
+
+def draw_window(p1, p2):
 
     screen.fill(BLACK)
-    screen.blit(player1_img, (0, 250))
-    screen.blit(player2_img, (850, 250))
+    screen.blit(player1_img, (p1.x, p1.y))
+    screen.blit(player2_img, (p2.x, p2.y))
     screen.blit(ball_img, (450, 250))
     pygame.display.update()
 
 def main():
+    p1 = pygame.Rect(0, 250, 25, 50)
+    p2 = pygame.Rect(870, 250, 25, 50)
 
     clock = pygame.time.Clock()
 
@@ -36,7 +44,19 @@ def main():
 
                 run = False
         
-        draw_window()
+        keys_pressed = pygame.key.get_pressed()
+
+        if keys_pressed[pygame.K_w]: # Player 1 going up
+            p1.y -= VEL
+        if keys_pressed[pygame.K_s]: # Player 1 going down
+            p1.y += VEL
+        
+        if keys_pressed[pygame.K_UP]: # Player 2 going up
+            p2.y -= VEL
+        if keys_pressed[pygame.K_DOWN]: # Player 2 going down
+            p2.y += VEL
+
+        draw_window(p1, p2)
 
     pygame.quit()
 
